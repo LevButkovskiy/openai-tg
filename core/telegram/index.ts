a7d7cccd5afb91a9a0140540241b03e0495cc370
ButkovskiyLev <lebutkovskiy@maximum.auto>
2023-06-17 21:00:17 +0300

    welcome message

--- core/telegram/index.ts
+++ core/telegram/index.ts
@@ -13,6 +13,7 @@ bot.on("message", (msg) => {
 	const chatId = msg.chat.id
 
 	if (!msg.text) return bot.sendMessage(chatId, "Ничего не введено")
+	if (msg.text === "/start") return bot.sendMessage(chatId, "Добро пожаловать в бота! \n Напишите ваш запрос")
 
 	if (!OPENAI_ENABLED) return bot.sendMessage(chatId, "ChatGPT пока отключен")
 
